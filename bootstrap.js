aeb1724cb4b16e436ac8dc12464fcbb4116646c2
diegocr <dcasorran@gmail.com>
2013-09-06 22:44:35 +0000

    added http observer

--- bootstrap.js
+++ bootstrap.js
@@ -11,7 +11,6 @@
 
 let {classes:Cc,interfaces:Ci,utils:Cu,results:Cr} = Components,addon;
 
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 Cu.import("resource://gre/modules/Services.jsm");
 
 function rsc(n) 'resource://' + addon.tag + '/' + n;
@@ -20,64 +19,56 @@ function LOG(m) (m = addon.name + ' Message @ '
 		dump(m + "\n"), Services.console.logStringMessage(m));
 
 let isMobile = ~['{aa3c5121-dab2-40e2-81ca-7ea25febc110}',
-	'{a23983c0-fd0e-11dc-95ff-0800200c9a66}'].indexOf(Services.appinfo.ID),
-	VOID = function() {};
+	'{a23983c0-fd0e-11dc-95ff-0800200c9a66}'].indexOf(Services.appinfo.ID);
 
 let i$ = {
-	QueryInterface: XPCOMUtils.generateQI([
-		Ci.nsISupports,
-		Ci.nsIWebProgressListener,
-		Ci.nsISupportsWeakReference]),
-	
-	handleEvent: function(ev) {
-		switch(ev.type) {
-			case 'TabOpen':
-				ev.target.addProgressListener(this);
-				break;
-			case 'TabClose':
-				ev.target.removeProgressListener(this);
-			default:
-				break;
-		}
-	},
-	onStateChange   : VOID,
-	onStatusChange  : VOID,
-	onProgressChange: VOID,
-	onSecurityChange: VOID,
-	onLocationChange: function(w,r,l) {
+	getLink: function(win,link) {
+		win = win.QueryInterface(Ci.nsIInterfaceRequestor)
+			.getInterface(Ci.nsIWebNavigation)
+			.QueryInterface(Ci.nsIDocShell)
+			.QueryInterface(Ci.nsIDocShellTreeItem).rootTreeItem
+			.QueryInterface(Ci.nsIInterfaceRequestor)
+			.getInterface(Ci.nsIDOMWindow)
+			.window;
 		
-		try {
-			
-			if(l.schemeIs("http") || l.schemeIs("https")) {
-				let win = w.DOMWindow.QueryInterface(Ci.nsIInterfaceRequestor)
-					.getInterface(Ci.nsIWebNavigation)
-					.QueryInterface(Ci.nsIDocShell)
-					.QueryInterface(Ci.nsIDocShellTreeItem).rootTreeItem
-					.QueryInterface(Ci.nsIInterfaceRequestor)
-					.getInterface(Ci.nsIDOMWindow)
-					.window, bro = win.diegocr[addon.tag];
+		let bro = win.diegocr[addon.tag],
+			clt = bro.cl(link);
+		
+		return (clt != link) ? (bro.blink(win), clt) : null;
+	},
+	observe: function(s,t,d) {
+		
+		switch(t) {
+			case 'http-on-examine-response': {
+				let c = s.QueryInterface(Ci.nsIHttpChannel);
 				
-				let c = bro.cl(l.spec);
-				
-				if(l.spec != c) {
-					r.cancel(Cr.NS_BINDING_REDIRECTED);
-					w.DOMWindow.location = c;
-					bro.blink(win);
-					
-					LOG('onLocationChange: ' + l.spec + "\n-> " + c);
+				try {
+					if(30 == parseInt(c.responseStatus/10) && 304 != c.responseStatus) {
+						let l = c.getResponseHeader('Location');
+						if(l) {
+							try {
+								var w = s.loadGroup.notificationCallbacks.getInterface(Ci.nsIDOMWindow);
+							} catch(e) {}
+							
+							if(w && (l = this.getLink(w,l))) {
+								
+								c.setResponseHeader('Location', l, false);
+							}
+						}
+					}
+				} catch(e) {
+					Cu.reportError(e);
 				}
-			}
-			
-		} catch(e) {
-			Cu.reportError(e);
+				
+			}	break;
 		}
 	},
 	onOpenWindow: function(aWindow) {
 		let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindow);
 		loadIntoWindowStub(domWindow);
 	},
-	onCloseWindow: VOID,
-	onWindowTitleChange: VOID
+	onCloseWindow: function() {},
+	onWindowTitleChange: function() {}
 };
 
 (function(global) global.loadSubScript = function(file,scope)
@@ -107,6 +98,9 @@ function loadIntoWindow(window) {
 	window.diegocr[addon.tag].LOG = LOG;
 	window.diegocr[addon.tag].rsc = rsc;
 	
+	if(isMobile)
+		return;
+	
 	let wmsData = {
 		TBBHandler: function(ev) {
 			
@@ -117,24 +111,6 @@ function loadIntoWindow(window) {
 			}
 		}
 	};
-	
-	if(addon.branch.getBoolPref('progltr')) {
-		wmsData.progltr = !0;
-		
-		if(isMobile) {
-			let BrowserApp = window.BrowserApp;
-			BrowserApp.tabs.forEach(function(tab) {
-				tab.browser.addProgressListener(i$);
-			});
-			BrowserApp.deck.addEventListener("TabOpen", i$, false);
-			BrowserApp.deck.addEventListener("TabClose", i$, false);
-		} else {
-			getBrowser(window).addProgressListener(i$);
-		}
-	}
-	
-	if(isMobile)
-		return;
 	
 	let gNavToolbox = window.gNavToolbox || $('navigator-toolbox');
 	if(gNavToolbox && gNavToolbox.palette.id == 'BrowserToolbarPalette') {
@@ -212,6 +188,9 @@ function unloadFromWindow(window) {
 		Cu.reportError(e);
 	}
 	
+	if(isMobile)
+		return;
+	
 	if(addon.wms.has(window)) {
 		let wmsData = addon.wms.get(window);
 		
@@ -222,22 +201,8 @@ function unloadFromWindow(window) {
 			let tt = $(addon.tag+'-tooltip');
 			if(tt) tt.removeEventListener('popupshowing', wmsData.popupshowing, !1);
 		}
-		if(wmsData.progltr) {
-			if(isMobile) {
-				window.BrowserApp.deck.removeEventListener("TabOpen", i$, false);
-				window.BrowserApp.deck.removeEventListener("TabClose", i$, false);
-				window.BrowserApp.tabs.forEach(function(tab) {
-					tab.browser.removeProgressListener(i$);
-				});
-			} else {
-				getBrowser(window).removeProgressListener(i$);
-			}
-		}
 		addon.wms.delete(window);
 	}
-	
-	if(isMobile)
-		return;
 	
 	if(btn) {
 		btn.parentNode.removeChild(btn);
@@ -256,14 +221,6 @@ function unloadFromWindow(window) {
 	let n;
 	if((n = $(addon.tag+'-tooltip')))
 		n.parentNode.removeChild(n);
-}
-
-function getBrowser(w) {
-	try {
-		return w.getBrowser();
-	} catch(e) {
-		return w.gBrowser;
-	}
 }
 
 function startup(data) {
@@ -312,6 +269,12 @@ function startup(data) {
 		}
 		wm.addListener(i$);
 		
+		if(addon.branch.getBoolPref('progltr')) {
+			addon.progltr = !0;
+			
+			Services.obs.addObserver(i$, 'http-on-examine-response', false);
+		}
+		
 		// i$.startup();
 		addon.branch.setCharPref('version', addon.version);
 	});
@@ -322,6 +285,9 @@ function shutdown(data, reason) {
 	if(reason == APP_SHUTDOWN)
 		return;
 	
+	if(addon.progltr) {
+		Services.obs.removeObserver(i$, 'http-on-examine-response', false);
+	}
 	// i$.shutdown();
 	
 	Services.wm.removeListener(i$);
