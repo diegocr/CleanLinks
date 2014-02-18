03957614f956a77f52de2bcac1f5c4576de49f74
Simon Chan <cnsimonchan@gmail.com>
2014-02-19 06:45:24 +0800

    Localization

--- bootstrap.js
+++ bootstrap.js
@@ -360,8 +360,8 @@ function loadIntoWindow(window) {
 								// e('separator'),
 								e('listbox',{flex:1,id:addon.tag+'-listbox',rows:10,seltype:'multiple',minheight:280},[
 									e('listhead',0,[
-										e('listheader',{label:'Original Link'}),
-										e('listheader',{label:'Cleaned Link'})
+										e('listheader',{label:_('bootstrap.listheader.original')}),
+										e('listheader',{label:_('bootstrap.listheader.cleaned')})
 									]),
 									e('listcols',0,[e('listcol'),e('listcol')])
 								]),
